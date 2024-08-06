8c4a81f7e2107c185f2ecb870233b9b0881767f1
Rithika <rithikasenthil5@gmail.com>
2024-08-06 11:24:15 +0530

    Uploads

---
+++ Rithika/JS/demo.js
@@ -0,0 +1,138 @@
+var factor="Surya"
+var fplayer="Virat"
+var fmovie ="Vikram"
+function favourite(){
+    console.log("Favourite Actor:",factor)
+    console.log("Favourite Player:",fplayer)
+    console.log("Favourite Movie:",fmovie)
+}
+favourite()
+
+
+
+//if else
+var spring=true
+if(spring){
+    console.log("enjoy the blooming flowers")
+}
+else if(summer){
+    console.log("have fun in the sun")
+}
+else if(autumn || fall)
+{
+    console.log("admire the colourfull leaves")
+}
+else if(winter)
+{
+    console.log("bundle up and stay warm")
+}
+
+
+var num=2
+if(num%0){
+    console.log("the number is even")
+}
+else{
+    console.log("the number is odd")
+}
+
+
+var alpha='r'
+if(alpha == 'a'|| alpha == 'e' || alpha == 'i'|| alpha == 'o'|| alpha =='u')
+{
+    console.log("its a vowel")
+}
+else{
+    console.log("its a constant")
+}
+
+
+var hw=true
+if(hw)
+{
+    console.log("Great job")
+}
+else{
+    console.log("Finish your homework before playing")
+}
+
+
+var raining=true;
+if(raining){
+    console.log("take an umbrella");
+}
+else{
+    console.log("enjoy ur sunshine");
+}
+
+
+var homework=false;
+if(homework)
+{
+    console.log("great job")
+}    
+else{
+    console.log("finish your homework before playing");
+}
+
+
+var cookies=false;
+if(cookies){
+    console.log("would you like a cookies")
+}
+else{
+    console.log("take to bake a cookie ")
+}
+
+
+var teeth=true
+if(teeth)
+{
+    console.log("your teeth are clean")
+}   
+else{
+    console.log("go brush and teeth")
+}
+
+/*for loop
+example*/
+for(count=1;count<=5;count=count+1)
+{
+    console.log('Rithu')
+}
+
+/*print numbers from 1 to 10 but increment by 2 in each step*/
+
+for(i=1;i<=10;i=i+2)
+{
+    console.log(i)
+}
+
+//print number in reverse order
+for(i=10;i>=1;i--)
+{
+    console.log(i)
+}
+
+ //print 2 tables
+for(i = 1;i<=10;i++)
+{
+      var c= 2*i
+      console.log("2 x",i, "=", c)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
